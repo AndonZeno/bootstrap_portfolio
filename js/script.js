b71b5c175b523fe4b4a7a1e188679af9f9ed79c7
Toni <tonizeno5@gmail.com>
2020-07-20 19:12:08 +0200

    Added about and skills section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
   var typed = new Typed(".typed", {
-    strings: ["Web Developer", "Telecom Engineer"],
+    strings: ["Web Developer", "Software Engineer"],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
     showCursor: false
   })
+
+  // sticky navbar
+
+  let nav_top_offset = $(".header_area").height() + 50
+
+  function navbarFixed() {
+    if ($(".header_area").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop()
+        if (scroll >= nav_top_offset) {
+          $(".header_area .main-menu").addClass("navbar_fixed")
+          $(".navbar").css("background-color", "white")
+        } else {
+          $(".header_area .main-menu").removeClass("navbar_fixed")
+        }
+      })
+    }
+  }
+
+  navbarFixed()
 })
